e110b1289b0a5d8f6f8574a5afede7501e543051
ssawrav <s.sawrav@gmail.com>
2022-06-01 03:41:47 +0545

    Add inital barebones for minion translator webapp
    Signed-off-by: ssawrav <s.sawrav@gmail.com>

---
+++ app.js
@@ -0,0 +1,9 @@
+var inputText = document.querySelector("#txt-input")
+var translateButton = document.querySelector("#btn-translate")
+var outputText = document.querySelector("#txt-output")
+
+function clickHandler() {
+    console.log("clicked!!")
+}
+
+translateButton.addEventListener("click", clickHandler)
