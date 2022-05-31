d64c0a047a43612607b1e3605605930345759f45
ssawrav <s.sawrav@gmail.com>
2022-06-01 04:35:50 +0545

    Make button functional again!!!
    - Jokes apart, the translation works
    
    Signed-off-by: ssawrav <s.sawrav@gmail.com>

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var inputText = document.querySelector("#txt-input")
 var translateButton = document.querySelector("#btn-translate")
 var outputText = document.querySelector("#txt-output")
+// var apiUrl = "https://api.funtranslations.com/translate/minion.json"
+var apiUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+
+function translateUrl(text) {
+    return apiUrl + "?text=" + text
+}
 
 function clickHandler() {
     console.log("clicked!!")
+
+    var textInput = inputText.value
+
+    fetch(translateUrl(textInput))
+    .then(response => response.json())
+    .then(json => {
+    var translatedText = json.contents.translated
+    outputText.innerText = translatedText})
 }
 
 translateButton.addEventListener("click", clickHandler)
